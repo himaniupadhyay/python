8a4b4b253a930cb68a4952f55716246fa46f65fd
himani-upadhyay <himani@infoud.co.in>
2017-12-28 12:41:55 +0530

    Merged

--- filtering.py
+++ filtering.py
@@ -1,5 +1,7 @@
+import json
+
 f = open('cities.json', 'r')
-message = f.read()
+message = f.readlines()
 str = 'Ahmedabad'
 k=0
 
@@ -8,10 +10,18 @@ k=0
 # else:
 #     print("No such string found,try again")
 for city in message:
-    if(city==str):
-        print str
-        print("Occurrences of the word:")
-        k= k+1
+    if str in city:
+        k = k+1
 f.close()
+print(k)
 
-
+# str = 'Ahmedabad'
+# k = 0
+# with open('gujarat.json', 'r') as f:
+#     items = json.load(f)
+#
+# for item in items:
+#     if item['c'].lower() == str.lower():
+#         k = k + 1
+#
+# print(k)
