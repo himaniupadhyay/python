2048aac1f5d5889d364d8b96b0f12cd0ca85895d
himani-upadhyay <himani@infoud.co.in>
2017-12-29 12:06:18 +0530

    Merge branch 'master' of https://github.com/himaniupadhyay/python
    # Conflicts:
    #	file-input.py
    #	filtering.py
    #	helloworld.txt

--- filtering.py
+++ filtering.py
@@ -1,27 +1,14 @@
 import json
 
-f = open('cities.json', 'r')
-message = f.readlines()
-str = 'Ahmedabad'
-k=0
 
-# if str in message:
-#     print(" Ahmedabad is Matched in Cities")
-# else:
-#     print("No such string found,try again")
-for city in message:
-    if str in city:
-        k = k+1
-f.close()
-print(k)
+def count_cities():
+    str = 'Ahmedabad'
+    k = 0
+    with open('cities.json', 'r') as f:
+        items = json.load(f)
 
-# str = 'Ahmedabad'
-# k = 0
-# with open('gujarat.json', 'r') as f:
-#     items = json.load(f)
-#
-# for item in items:
-#     if item['c'].lower() == str.lower():
-#         k = k + 1
-#
-# print(k)
+    for item in items:
+        if item['c'].lower() == str.lower():
+            k = k + 1
+
+    return k
